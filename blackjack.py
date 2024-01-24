676881e6b5b4fb93bbaeec43dda658fea594fa3b
London Haith <136548743+thelondon2002@users.noreply.github.com>
2024-01-24 17:47:48 -0600

    intial commit
    added initial commit

---
+++ blackjack.py
@@ -0,0 +1,43 @@
+import random
+
+# the name of the game is blackjack
+
+# user, dealer
+
+# deck of cards with values (2 - 10, J,K,Q = 10, A = 1/11)
+
+# user given 1 card, dealer next, user, then dealer
+
+# user, dealer given option to hit or stand
+
+# random generator of certain amount of cards being dealt
+
+# if Q, J, K paired with A = automatic WindowsError
+
+# balance? (later)
+
+class Card:
+    pass
+
+class Deck:
+    pass
+
+
+class Hand:
+    pass
+
+
+class User:
+    pass
+
+
+class Dealer:
+    pass
+
+
+class Hit:
+    pass
+
+
+class Stand:
+    pass
