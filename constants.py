b4ee0269cf33713cd469dca69915b8c63ea8b95b
London Haith <136548743+thelondon2002@users.noreply.github.com>
2024-02-01 23:03:26 -0600

    Update constants.py

--- constants.py
+++ constants.py
@@ -7,21 +7,22 @@ display_width = 900
 display_height = 700
 pygame.init()
 background_color = (0,110,34)
-grey = (220,220,220)
+grey = (92, 90, 84)
 black = (0,0,0)
 green = (0, 200, 0)
 red = (255,0,0)
 light_slat = (255, 255, 255)
 dark_slat = (220,220,220)
 dark_red = (255, 0, 0)
-font = pygame.font.SysFont("Arial", 20)
-textfont = pygame.font.SysFont('Comic Sans MS', 35)
+font = pygame.font.SysFont("impact", 20)
+textfont = pygame.font.SysFont('Arial', 35)
 game_end = pygame.font.SysFont('dejavusans', 100)
-blackjack = pygame.font.SysFont('Arial', 70)
+blackjack = pygame.font.SysFont('couriernew', 70)
+
 
 script_dir = os.path.dirname(os.path.abspath(__file__))
 
-background_image_path = os.path.join(script_dir, "img", "background.jpg")
+background_image_path = os.path.join(script_dir, "img", "background.png")
 background_image = pygame.image.load(background_image_path)
 background_image = pygame.transform.scale(background_image, (display_width, display_height))
 
@@ -29,7 +30,8 @@ background_image = pygame.transform.scale(background_image, (display_width, disp
 SUITS = ['C', 'S', 'H', 'D']
 RANKS = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
 
-CARD_WIDTH = 100
-CARD_HEIGHT = 120
+CARD_WIDTH = 130
+CARD_HEIGHT = 150
+
 
 
